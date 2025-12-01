e1b02e7f40befa9c27db349be39ec80f4e61c3d7
Roggerrs <rogger6412@gmail.com>
2025-12-01 18:36:09 -0300

    Sistema TVsmart

---
+++ meu-primeiro-projeto-java/src/Usuario.java
@@ -0,0 +1,39 @@
+public class Usuario {
+    
+public static void main(String[] args) {
+    
+   SmartTv smartTV = new SmartTv();
+   
+smartTV.diminuirVolume();
+smartTV.diminuirVolume();
+smartTV.diminuirVolume();
+smartTV.aumentarVolume();
+
+
+System.out.println("Canal Atual :" +smartTV.canal);
+smartTV.mudarCanal(13);
+System.out.println("Canal Atual :" +smartTV.canal);
+System.out.println("Canal Atual :" +smartTV.canal);
+
+
+
+
+System.out.println("Volume Atual : " +smartTV.volume);
+System.out.println("TV Ligada ? " + smartTV.ligada);
+
+
+
+
+System.out.println("Canal Atual :" +smartTV.canal);
+System.out.println("Volume Atual : " +smartTV.volume);
+
+smartTV.ligar();
+System.out.println("Novo Status -> TV Ligada ? " + smartTV.ligada);   
+
+smartTV.desligar();
+System.out.println("Novo Status -> TV Ligada ? " + smartTV.ligada); 
+
+}    
+
+
+}
